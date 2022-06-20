505206d75554431efaa467104557372054f41e27
Muhammad Fajar <m.fajar980527@gmail.com>
2022-06-20 16:58:25 +0700

    disable origin

--- src/main/java/com/example/shonicserver/controller/DummyController.java
+++ src/main/java/com/example/shonicserver/controller/DummyController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import org.json.HTTP;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/mock")
+@CrossOrigin(origins = "*")
 @Api(tags = "Data Dummy")
 public class DummyController {
 
